8fb23992de75445895c6c25cf22af5ce150ce239
prochnost04 <aks3555@gmail.com>
2023-09-03 14:06:36 +0530

    confeti fix

--- src/Component/thankyou/ThankYou.js
+++ src/Component/thankyou/ThankYou.js
@@ -3,9 +3,9 @@ import './thankyou.css';
 import Lock from '../assets/shoppingBag.png';
 import UserContext, { EMPTY_USER } from '../../context/userContext';
 import RedirectionBar from '../RedirectionBar/RedirectionBar';
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import { CONFETI, SNOW } from '../../utils/particlePresets';
+import { CONFETI } from '../../utils/particlePresets';
+import ParticlesComponent from '../Particles/ParticlesComponent';
+
 function ThankYou() {
     const { setUser } = useContext(UserContext);
 
@@ -13,16 +13,6 @@ function ThankYou() {
         setUser(EMPTY_USER);
     }, []);
 
-    const particlesInit = async (main) => {
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets 
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready 
-        // starting from v2 you can add only the features you need reducing the bundle size 
-        await loadFull(main);
-    };
-
-    const particlesLoaded = (container) => {
-        console.log(container);
-    };
 
     return (
         <>
@@ -35,11 +25,8 @@ function ThankYou() {
                 <h3 className='prize-content'>P.S: Don't leave without your prize!</h3>
                 <img src={Lock} alt='lock' className='lock-img' />
             </div>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={CONFETI}
+            <ParticlesComponent
+                particlePreset={CONFETI}
             />
         </>
 
